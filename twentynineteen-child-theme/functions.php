9c2ae4526e1e093cd7034cadd1d9aaaed44b95fc
Joe Casabona <jcasabona@gmail.com>
2019-06-14 11:50:59 -0400

    8:2 End State

--- twentynineteen-child-theme/functions.php
+++ twentynineteen-child-theme/functions.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once( get_stylesheet_directory() . '/lil-post-types/lil-post-types.php' );
+
 function lil_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style') );    
