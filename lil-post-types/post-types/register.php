c58c6e30be4115adee2e0d348afa43d9028cef2a
Joe Casabona <jcasabona@gmail.com>
2019-06-14 11:36:39 -0400

    4:4 End State

--- lil-post-types/post-types/register.php
+++ lil-post-types/post-types/register.php
@@ -5,9 +5,9 @@ function lil_register_business_type() {
         'name' => __( 'Businesses', LILDOMAIN ),
         'singular_name' => __( 'Business', LILDOMAIN ),
         'featured_image' => __( 'Business Logo', LILDOMAIN ),
-        'set_feature_image' => __( 'Set Business Logo', LILDOMAIN ),
-        'remove_feature_image' => __( 'Remove Logo', LILDOMAIN ),
-        'use_feature_image' => __( 'Use Logo', LILDOMAIN ),
+        'set_featured_image' => __( 'Set Business Logo', LILDOMAIN ),
+        'remove_featured_image' => __( 'Remove Logo', LILDOMAIN ),
+        'use_featured_image' => __( 'Use Logo', LILDOMAIN ),
         'archives' => __( 'Business Directory', LILDOMAIN ),
         'add_new' => __( 'Add New Business', LILDOMAIN ),
         'add_new_item' => __( 'Add New Business', LILDOMAIN ),
@@ -16,6 +16,10 @@ function lil_register_business_type() {
     $args = array( 
         'labels' => $labels,
         'public' => true,
+        'has_archive' => 'businesses',
+        'rewrite' => array( 'has_front' => true ),
+        'menu_icon' => 'dashicons-building',
+        'supports' => array( 'title', 'editor', 'thumbnail' ),
     );
 
     register_post_type( 'business', $args );
