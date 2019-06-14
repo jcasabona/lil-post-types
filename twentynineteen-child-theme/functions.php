6850861dd8da50467a3761f4489c5362b900e8f4
Joe Casabona <jcasabona@gmail.com>
2019-06-14 11:48:56 -0400

    7:3 End State

--- twentynineteen-child-theme/functions.php
+++ twentynineteen-child-theme/functions.php
@@ -13,3 +13,29 @@ function lil_add_business_to_query( $query ) {
 }
 
 add_action( 'pre_get_posts', 'lil_add_business_to_query' );
+
+function lil_show_events() {
+    $args = array( 
+        'post_type' => 'event',
+        'posts_per_page' => 3,
+    );
+
+    $events = new WP_Query( $args );
+
+    if ( $events->have_posts() ) {
+        echo '<ul class="events-list">';
+        $format = '<li class="event"><a href="%1$s" title="%2$s">%2$s</a>: %3$s</li>';
+
+        while( $events->have_posts() ) {
+            $events->the_post(); 
+            printf( $format, 
+                get_permalink(), 
+                get_the_title(), 
+                apply_filters( 'the_content', get_the_content() )
+            );
+        }
+        echo '</ul>';
+    }
+
+    wp_reset_query();
+}
